bda5c0da55c3ab418830a67cae49f440c32e0099
Michał Mietła <mrbroomchannel@gmail.com>
2022-09-07 13:53:10 +0200

    Fix non-displaying initial value in AmountWidget

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -34,6 +34,7 @@ class AmountWidget extends BaseWidget {
   initActions() {
     const thisWidget = this;
 
+    thisWidget.value = settings.amountWidget.defaultValue;
     thisWidget.dom.input.addEventListener('change', function () { thisWidget.setValue(thisWidget.dom.input.value); });
     thisWidget.dom.linkDecrease.addEventListener('click', function (event) {
       event.preventDefault();
